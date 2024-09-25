20fb04cf2c909df31637e6ac942fcb744f1c944d
yanis bensaid <yanis.ben-said@epitech.eu>
2024-09-25 16:41:40 +0200

    [UPDATE] : navigation barre functionnal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ const menuItems = [
   { title: 'Accueil', link: '/' },
   { title: 'Parents et jeunes', link: '/Parents_et_jeunes' },
   { title: 'Entreprises et associations', link: '/Entreprises_et_associations' },
-  { title: 'Institutions et enseignants', link: '/Institutions_et_enseignants' }
+  { title: 'Institutions et enseignants', link: '/Institutions_et_enseignants' },
+  { title: "Découvrez les Entretiens de l'Excellence", link: "/Découvrez_les_Entretiens_de_l'Excellence" }
+
 ];
 
 function App() {
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const NavbarContainer = styled.nav`
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  padding: 1rem;
-  background-color: #333;
+  justify-content: center;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 2.0rem;
+  background-color: rgba(0, 0, 247, 1);
   color: white;
+  margin: 0;
 `;
 
 const NavItem = styled.a`
   margin: 0 1rem;
+  margin-top: 5rem;
+  margin-bottom: -2rem;
+  padding: 0.5rem 1rem;
   color: white;
   text-decoration: none;
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.2rem;
+  white-space: nowrap;
+  transition: background-color 0.2s;
+  border-bottom: 3px solid transparent;
+  border-radius: 1px;
   &:hover {
-    text-decoration: underline;
+    background-color: #585bfd;
+    color: #fecd3e;
+    border-bottom: 3px solid #fecd3e;
   }
 `;
 
